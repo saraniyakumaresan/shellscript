5bb5d763efed65d41d927a72e28458a90f7babd7
saraniyakumaresan <119513623+saraniyakumaresan@users.noreply.github.com>
2023-12-13 11:34:43 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 echo "Welcome"
 ls "/"
 echo "These are list of root dir"
-echo "##gbStart##vault##splitKeyValue##testing##gbEnd##"
-echo "##gbStart##vault1##splitKeyValue##testing1##gbEnd##"
+echo "##gbStart##vault2##splitKeyValue##testing##gbEnd##"
+echo "##gbStart##vault3##splitKeyValue##testing1##gbEnd##"
