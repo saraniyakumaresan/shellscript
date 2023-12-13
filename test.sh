9d9d67b0674b74188c10e041ce9da8baf22c180b
saraniyakumaresan <119513623+saraniyakumaresan@users.noreply.github.com>
2023-12-13 11:00:51 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Welcome"
+ls "/"
+echo "These are list of root dir"
+echo "##gbStart##vault##splitKeyValue##testing##gbEnd##"
+echo "##gbStart##vault1##splitKeyValue##testing1##gbEnd##"
